bc65db613398e61f5b82fcf660b1c0eafce2ef59
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-07-17 12:26:19 -0500

    minor adj to header/nav-items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,33 +5,27 @@ import './css/Header.css'
 
 const Header = () => {
     return (
-      <>
-      IMage Here
-      <>
-      </>
-      <Nav className="d-flex, align-items-center justify-content-center" 
-         activeKey="/home"
-         onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
-        <Nav.Item>
-          <Nav.Link><Link to="/">Home</Link></Nav.Link>
-        </Nav.Item>
-
-        <Nav.Item>
-          <Nav.Link><Link to="/about-us">About Us</Link></Nav.Link>
-        </Nav.Item>
-        
-        <Nav.Item>
-          <Nav.Link><Link to="/gallery">Gallery</Link></Nav.Link>
-        </Nav.Item>
-
-        <Nav.Item>
-          <Nav.Link><Link to="/events">Events</Link></Nav.Link>
-        </Nav.Item>
-
-        <Nav.Item>
-          <Nav.Link><Link to="/contact_us">Contact Us</Link></Nav.Link>
-        </Nav.Item>
-      </Nav>
+     <>
+      <img src="/img/bayou-bros-logo.png" alt="logo"></img>
+        <Nav className="d-flex, align-items-center justify-content-center" 
+          activeKey="/home"
+          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+          <Nav.Item>
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+           </Nav.Item>
+          <Nav.Item>
+            <Nav.Link><Link to="/about-us">About Us</Link></Nav.Link>
+           </Nav.Item>
+          <Nav.Item>
+            <Nav.Link><Link to="/gallery">Gallery</Link></Nav.Link>
+           </Nav.Item>
+          <Nav.Item>
+            <Nav.Link><Link to="/events">Events</Link></Nav.Link>
+           </Nav.Item>
+          <Nav.Item>
+            <Nav.Link><Link to="/contact_us">Contact Us</Link></Nav.Link>
+          </Nav.Item>
+        </Nav>
       </>
     )
 }
