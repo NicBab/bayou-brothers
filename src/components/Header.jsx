4aad3d1d1e20e1d8d61bba760ba0482bcd8abea5
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-07-17 13:27:59 -0500

    nav routes adj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,19 +8,18 @@ const Header = () => {
      <>
       <img src="/img/bayou-bros-logo.png" alt="logo"></img>
         <Nav className="d-flex, align-items-center justify-content-center" 
-          activeKey="/home"
-          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+          activeKey="/home">
           <Nav.Item>
             <Nav.Link><Link to="/">Home</Link></Nav.Link>
-           </Nav.Item>
-          <Nav.Item>
-            <Nav.Link><Link to="/about-us">About Us</Link></Nav.Link>
            </Nav.Item>
           <Nav.Item>
             <Nav.Link><Link to="/gallery">Gallery</Link></Nav.Link>
            </Nav.Item>
           <Nav.Item>
             <Nav.Link><Link to="/events">Events</Link></Nav.Link>
+           </Nav.Item>
+           <Nav.Item>
+            <Nav.Link><Link to="/about-us">About Us</Link></Nav.Link>
            </Nav.Item>
           <Nav.Item>
             <Nav.Link><Link to="/contact_us">Contact Us</Link></Nav.Link>
